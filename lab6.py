384b23cfb060b1111c0005c2e324379f1abeee16
imagallanes24 <89544214+imagallanes24@users.noreply.github.com>
2023-03-09 17:58:18 -0800

    Update lab6.py

--- lab6.py
+++ lab6.py
@@ -13,15 +13,15 @@ grove_rgb_lcd.setRGB(255, 255, 255)
 grove_rgb_lcd.setText("Threshold: ", "")
 
 while True:
-  threshold_raw = grovepi.analogRead(rotary_angle_sensor_port)
-  threshold = int(grovepi.map(threshold_raw, 0, 1023, threshold_min, threshold_max))
-  distance = grovepi.ultrasonicRead(ultrasonic_ranger_port)
-  object_present = distance < threshold
-  
-  top_line = f"Threshold: {threshold}"
-  if object_present:
-    top_line += " OBJ PRES"
-  bottom_line = f"Distance: {distance}"
-  grove_rgb_lcd.setText(top_line, bottom_line)
-  
-  grovepi.delay(50)
+    threshold_raw = grovepi.analogRead(rotary_angle_sensor_port)
+    threshold = int(grovepi.map(threshold_raw, 0, 1023, threshold_min, threshold_max))
+    distance = grovepi.ultrasonicRead(ultrasonic_ranger_port)
+    object_present = distance < threshold
+
+    top_line = f"Threshold: {threshold}"
+    if object_present:
+        top_line += " OBJ PRES"
+    bottom_line = f"Distance: {distance}"
+    grove_rgb_lcd.setText(top_line, bottom_line)
+
+    grovepi.delay(50)
